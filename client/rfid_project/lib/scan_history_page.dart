96b4fa132a73ba3e736c4dda1a93d89f06114c1c
neelmoni1 <95948086+neelmoni1@users.noreply.github.com>
2023-11-01 10:32:12 -0500

    Update scan_history_page.dart

--- client/rfid_project/lib/scan_history_page.dart
+++ client/rfid_project/lib/scan_history_page.dart
@@ -1,14 +1,108 @@
 import 'package:flutter/material.dart';
 
-class ScanHistoryPage extends StatelessWidget {
+class TrendsPage extends StatefulWidget {
+  @override
+  _TrendsPageState createState() => _TrendsPageState();
+}
+
+class ProductInfo {
+  final String id;
+  final String name;
+  final String imageUrl;
+  final String dimensions;
+  final String description;
+
+  ProductInfo({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.dimensions,
+    required this.description,
+  });
+}
+
+class _TrendsPageState extends State<TrendsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Scan History'),
+        title: Text('Product Page'),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              if (value == 'product') {
+                // Handle Product Page option
+              } else if (value == 'history') {
+                // Handle Scan History option
+              } else if (value == 'trends') {
+                // Handle Trends option
+              } else if (value == 'orders') {
+                // Handle Orders Page option
+              }
+            },
+            itemBuilder: (BuildContext context) {
+              return {
+                'Product Page',
+                'Scan History',
+                'Trends',
+                'Orders Page',
+              }.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Scan History Content'),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Product Page',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                  columns: [
+                    DataColumn(label: Text('Gate ID', style: TextStyle(fontWeight: FontWeight.bold))),
+                    DataColumn(label: Text('Product ID', style: TextStyle(fontWeight: FontWeight.bold))),
+                    DataColumn(label: Text('Product Name', style: TextStyle(fontWeight: FontWeight.bold))),
+                    DataColumn(label: Text('Action', style: TextStyle(fontWeight: FontWeight.bold))),
+                    DataColumn(label: Text('Timestamp', style: TextStyle(fontWeight: FontWeight.bold))),
+                  ],
+                  rows: [
+                    DataRow(cells: [
+                      DataCell(
+                        Tooltip(
+                          message: 'Gate 1',
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Handle button click
+                            },
+                            child: Text('1'),
+                          ),
+                        ),
+                      ),
+                      DataCell(Text('Gate 1')),
+                      DataCell(Text('234080941')),
+                      DataCell(Text('Pencil Case')),
+                      DataCell(Text('Incoming')),
+                      DataCell(Text('ISO TIMESTAMP')),
+                    ]),
+                    // ... other rows
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
