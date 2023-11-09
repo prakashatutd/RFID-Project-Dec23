f7633af78799420464ac485995988e9b652d3c56
neelmoni1 <95948086+neelmoni1@users.noreply.github.com>
2023-11-09 15:01:31 -0600

    Update trends_page.dart

--- server/dashboard/lib/trends_page.dart
+++ server/dashboard/lib/trends_page.dart
@@ -39,7 +39,7 @@ class TrendsPage extends StatelessWidget {
       body: Center(
         child: Container(
         padding: const EdgeInsets.all(10),
-        width: double.infinity,
+        width: 1500,
         height: 500,
         child: LineChart(
           LineChartData(borderData: FlBorderData(show: false), lineBarsData: [
@@ -49,11 +49,20 @@ class TrendsPage extends StatelessWidget {
               const FlSpot(2, 10),
               const FlSpot(3, 7),
               const FlSpot(4, 12),
-              const FlSpot(5, 13),
-              const FlSpot(6, 17),
-              const FlSpot(7, 15),
-              const FlSpot(8, 20)
-            ])
+              const FlSpot(10, 13),
+            ], color: Colors.blue,),
+            LineChartBarData(spots: [
+              const FlSpot(0, 1),
+              const FlSpot(2, 2),
+              const FlSpot(5, 15),
+              const FlSpot(10, 20)
+            ], color: Colors.red),
+            LineChartBarData(spots: [
+              const FlSpot(0, 0),
+              const FlSpot(4, 5),
+              const FlSpot(5, 5),
+              const FlSpot(10, 1)
+            ], color: Colors.green),
           ]),
         ),
         ),
