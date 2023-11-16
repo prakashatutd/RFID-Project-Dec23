2c8e87ff5e806cde214c8c2e94346b31baf7a1fa
Devansh Shah <devanshshah246@gmail.com>
2023-11-16 13:40:43 -0600

    Removed edit function and added product description

--- server/dashboard/lib/product_page.dart
+++ server/dashboard/lib/product_page.dart
@@ -24,56 +24,9 @@ class ProductInfo {
 class _ProductPageState extends State<ProductPage> {
   // Mock data for demonstration purposes
   List<ProductInfo> products = [
-    ProductInfo(id: '1', name: 'Product 1', imageUrl: '', dimensions: '', description: ''),
+    ProductInfo(id: '1', name: 'Product 1', imageUrl: '', dimensions: '', description: 'Description for Product 1'),
     // ... other products
   ];
-
-  // Function to edit table data
-  void _editTableData() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Edit Table Data'),
-          content: Column(
-            children: products.map((product) {
-              // Create text editing controllers for each field
-              TextEditingController idController = TextEditingController(text: product.id);
-              TextEditingController nameController = TextEditingController(text: product.name);
-              TextEditingController imageUrlController = TextEditingController(text: product.imageUrl);
-              TextEditingController dimensionsController = TextEditingController(text: product.dimensions);
-              TextEditingController descriptionController = TextEditingController(text: product.description);
-
-              return Column(
-                children: [
-                  // Text field for Product ID
-                  TextField(controller: idController, decoration: InputDecoration(labelText: 'Product ID')),
-                  // Text field for Product Name
-                  TextField(controller: nameController, decoration: InputDecoration(labelText: 'Product Name')),
-                  // Text field for Image URL
-                  TextField(controller: imageUrlController, decoration: InputDecoration(labelText: 'Image URL')),
-                  // Text field for Dimensions
-                  TextField(controller: dimensionsController, decoration: InputDecoration(labelText: 'Dimensions')),
-                  // Text field for Description
-                  TextField(controller: descriptionController, decoration: InputDecoration(labelText: 'Description')),
-                  SizedBox(height: 20),
-                ],
-              );
-            }).toList(),
-          ),
-          actions: [
-            ElevatedButton(
-              onPressed: () {
-                // Handle save/edit logic here
-                Navigator.pop(context);
-              },
-              child: Text('Save'),
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,7 +35,7 @@ class _ProductPageState extends State<ProductPage> {
         title: Text('Product Page'),
         actions: <Widget>[
           PopupMenuButton<String>(
-             onSelected: (value) {
+            onSelected: (value) {
               if (value == 'Product Page') {
                 Navigator.pushNamed(context, '/product');
               } else if (value == 'Scan History') {
@@ -119,31 +72,61 @@ class _ProductPageState extends State<ProductPage> {
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _editTableData,
-              child: Text('Edit Table Data'),
-            ),
-            SizedBox(height: 20),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: DataTable(
-                columns: [
-                  DataColumn(label: Text('Product ID', style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('Product Name', style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('Min Storage Quantity', style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('Resupply Threshold', style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('Current Quantity', style: TextStyle(fontWeight: FontWeight.bold))),
-                ],
-                rows: products.map((product) {
-                  return DataRow(cells: [
-                    DataCell(Text(product.id)),
-                    DataCell(Text(product.name)),
-                    DataCell(Text(product.imageUrl)),
-                    DataCell(Text(product.dimensions)),
-                    DataCell(Text(product.description)),
-                  ]);
-                }).toList(),
-              ),
+            Row(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(
+                      columns: [
+                        DataColumn(label: Text('Product ID', style: TextStyle(fontWeight: FontWeight.bold))),
+                        DataColumn(label: Text('Product Name', style: TextStyle(fontWeight: FontWeight.bold))),
+                        DataColumn(label: Text('Min Storage Quantity', style: TextStyle(fontWeight: FontWeight.bold))),
+                        DataColumn(label: Text('Resupply Threshold', style: TextStyle(fontWeight: FontWeight.bold))),
+                        DataColumn(label: Text('Current Quantity', style: TextStyle(fontWeight: FontWeight.bold))),
+                      ],
+                      rows: products.map((product) {
+                        return DataRow(cells: [
+                          DataCell(
+                            Text(
+                              product.id,
+                              style: TextStyle(
+                                color: Colors.blue,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ),
+                          DataCell(Text(product.name)),
+                          DataCell(Text(product.imageUrl)),
+                          DataCell(Text(product.dimensions)),
+                          DataCell(Text(product.description)),
+                        ]);
+                      }).toList(),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Product Description',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Description for Product 1',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
