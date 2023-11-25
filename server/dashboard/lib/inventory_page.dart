910fb8533a55aa867989636f59fa9b92966fb888
Arsen Yang <aby190001@utdallas.edu>
2023-11-24 21:29:41 -0600

    Convert inventory DataTable to PaginatedDataTable2

--- server/dashboard/lib/inventory_page.dart
+++ server/dashboard/lib/inventory_page.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-
-class InventoryPage extends StatefulWidget {
-  const InventoryPage({super.key});
-
-  @override
-  _InventoryPageState createState() => _InventoryPageState();
-}
+import 'package:data_table_2/data_table_2.dart';
 
 // Basic product info shown on inventory page
 class ProductBasicInfo {
@@ -43,74 +37,72 @@ class ProductDetailedInfo {
   }
 }
 
+class InventoryDataSource extends DataTableSource {
+  final List<ProductBasicInfo> products = <ProductBasicInfo>[
+    ProductBasicInfo(111222333, 'BIC Soft Feel Ballpoint Pen 25ct', 'Pens', 25, 40),
+    ProductBasicInfo(777555444, 'X-ACTO Quiet Pro 35ct', 'Pencil Sharpeners', 5, 2),
+    ProductBasicInfo(444999000, 'Crayola Washable Markers 100ct', 'Markers', 15, 30),
+    ProductBasicInfo(444111555, 'Fresh Oranges 60ct', 'Produce', 20, 57),
+  ];
+
+  @override
+  int get rowCount => products.length;
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get selectedRowCount => 0;
+
+  @override
+  DataRow? getRow(int index) {
+    if (index < 0 || index > rowCount)
+      return null;
+
+    final ProductBasicInfo product = products[index];
+
+    return DataRow(
+      cells: <DataCell>[
+        DataCell(Text(product.id.toString())),
+        DataCell(Text(product.name)),
+        DataCell(Text(product.category)),
+        DataCell(Text(product.resupplyThreshold.toString())),
+        DataCell(Text(product.quantity.toString())),
+      ],
+    );
+  }
+}
+
+final InventoryDataSource inventoryDataSource = InventoryDataSource();
+
+class InventoryPage extends StatefulWidget {
+  const InventoryPage({super.key});
+
+  @override
+  _InventoryPageState createState() => _InventoryPageState();
+}
+
 class _InventoryPageState extends State<InventoryPage> {
-  // Mock data for demonstration purposes
-  Map<int, ProductBasicInfo> basicProducts = {
-    111222333 : ProductBasicInfo(111222333, 'BIC Soft Feel Ballpoint Pen 25ct', 'Pens', 25, 40),
-    777555444 : ProductBasicInfo(777555444, 'X-ACTO Quiet Pro 35ct', 'Pencil Sharpeners', 5, 2),
-    444999000 : ProductBasicInfo(444999000, 'Crayola Washable Markers 100ct', 'Markers', 15, 30),
-  };
-  Map<int, ProductDetailedInfo> detailedProducts = {};
+  int _rowsPerPage = PaginatedDataTable.defaultRowsPerPage;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Expanded(
-            child: SingleChildScrollView(
-              child: DataTable(
-                columns: [
-                  DataColumn(label: Text('Product ID', style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('Name', style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('Category', style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('Resupply Threshold', style: TextStyle(fontWeight: FontWeight.bold))),
-                  DataColumn(label: Text('Quantity', style: TextStyle(fontWeight: FontWeight.bold))),
-                ],
-                rows: basicProducts.values.map((basicProduct) {
-                  return DataRow(
-                    cells: [
-                      DataCell(
-                        Text(
-                          basicProduct.id.toString(),
-                          style: TextStyle(
-                            color: Colors.blue,
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      ),
-                      DataCell(Text(basicProduct.name)),
-                      DataCell(Text(basicProduct.category)),
-                      DataCell(Text(basicProduct.resupplyThreshold.toString())),
-                      DataCell(Text(basicProduct.quantity.toString())),
-                    ]);
-                }).toList(),
-              ),
-            ),
-          ),
-          Container(
-            width: 200,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black),
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-            ),
-            padding: EdgeInsets.all(8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Product Description',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'Defintely one of the products of all time!',
-                ),
-              ],
-            ),
-          ),
+      padding: const EdgeInsets.all(16.0),      
+      child: PaginatedDataTable2(
+        availableRowsPerPage: <int>[10, 20, 30],
+        rowsPerPage: _rowsPerPage,
+        onRowsPerPageChanged: (int? value) {
+          _rowsPerPage = value!;
+        },
+        showFirstLastButtons: true,
+        source: inventoryDataSource,
+        columns: const <DataColumn>[
+          DataColumn(label: Text('Product ID', style: TextStyle(fontWeight: FontWeight.bold))),
+          DataColumn(label: Text('Name', style: TextStyle(fontWeight: FontWeight.bold))),
+          DataColumn(label: Text('Category', style: TextStyle(fontWeight: FontWeight.bold))),
+          DataColumn(label: Text('Resupply Threshold', style: TextStyle(fontWeight: FontWeight.bold))),
+          DataColumn(label: Text('Quantity', style: TextStyle(fontWeight: FontWeight.bold))),
         ],
       ),
     );
