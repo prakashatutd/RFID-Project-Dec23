8671d9fe73c985f6782515917208e5c65b490038
Arsen Yang <aby190001@utdallas.edu>
2024-01-30 00:20:59 -0600

    Add comments to firmware

--- device/RFIDGate/RFIDGate.ino
+++ device/RFIDGate/RFIDGate.ino
@@ -4,27 +4,32 @@
 #include "SparkFun_UHF_RFID_Reader.h"
 #include "TagCache.h"
 
-#define EPC_OFFSET 31
+#define EPC_OFFSET 31          // offset of EPC data in the raw data returned by SRTR
+#define TAG_WRITE_TIMEOUT 5000 // how long to try to write to a tag for in ms
+#define TAG_READ_TIMEOUT  5000
+
+// Wireless connection constants
 #define WIFI_SSID "RFID-ICS"
 #define WIFI_PASSWORD "RFID-ICS"
 #define INTERNAL_ID "1"
 #define SERVER_PORT 8000
 
+// Buzzer ports
 #define BUZZER1 9
 #define BUZZER2 10
 
-// WiFi variables
+// Wireless connection global variables
 IPAddress serverIP(192, 168, 1, 133);
 WiFiClient wifiClient;
 unsigned long lastMessageTime = 0;
 
-// RFID variables
-bool debug = false;
-bool gateReading = false;
-TagCache<32> tagCache;
-RFID nano;
+// RFID module-related global variables
+bool debug = false;       // whether debug output from RFID module is enabled
+bool gateReading = false; // whether the module is continuous read mode
+TagCache<32> tagCache;    // 32-entry tag cache, see below for details
+RFID nano;                // module interface object
 
-// Establish connection between Arduino and RFID module at specified baud rate
+// Establish a connection between Arduino and RFID module at specified baud rate
 // Return true if connection established, false otherwise
 bool setupNano(long baudRate)
 {
@@ -35,9 +40,9 @@ bool setupNano(long baudRate)
 
   nano.begin(Serial1);
   Serial1.begin(baudRate);
-  while (!Serial1); // Wait for port to open
+  while (!Serial1); // wait for port to open
 
-  //About 200ms from power on module will send its firmware version at 115200bps, ignore it
+  // About 200ms from power on module will send its firmware version at 115200bps, ignore it
   while (Serial1.available())
     Serial1.read();
 
@@ -47,7 +52,7 @@ bool setupNano(long baudRate)
   {
     // Wrong opcode indicates that module is already doing a continuous read, so tell it to stop
     nano.stopReading();
-    Serial.println("Module continuously reading. Asking it to stop...");
+    Serial.println("RFID module continuously reading. Asking it to stop...");
     delay(1500);
   }
   else
@@ -72,6 +77,8 @@ bool setupNano(long baudRate)
 }
 
 // Attempt to connect to local network
+// Return true if connected, false if no WiFi module found
+// Note that this function will block until it connects
 bool connectWifi()
 {
   if (WiFi.status() == WL_NO_MODULE)
@@ -94,7 +101,9 @@ bool connectWifi()
   return true;
 }
 
-void sendProductCodes()
+// Send an HTTP PATCH message to the back-end containing scanned product info
+// INCOMPLETE
+void sendProductInfo()
 {
   //wifiClient.println("PATCH /api/gate/scan/ HTTP/1.0");
   //wifiClient.println("{\"internal_id\":\"0\",");
@@ -102,9 +111,9 @@ void sendProductCodes()
   for (size_t i = 0; i < tagCache.capacity(); ++i)
   {
     uint32_t productId;
-    uint32_t productCode;
+    uint32_t productSerialNumber;
 
-    if (!tagCache.remove(i, &productId, &productCode))
+    if (!tagCache.remove(i, &productId, &productSerialNumber))
       continue;
     
     //wifiClient.print("\"");
@@ -117,17 +126,20 @@ void sendProductCodes()
   wifiClient.println();
 }
 
+// Send a keep alive message to the back-end
 void sendKeepAlive()
 {
   wifiClient.println("PATCH /api/gate/status/ HTTP/1.0");
   wifiClient.println("{\"internal_id\":\"0\"}");
 }
 
+// Run after Arduino powers on
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial); // Wait for port to open
+  while (!Serial); // wait for port to open
 
+  // Configure buzzer pins
   pinMode(BUZZER1, OUTPUT);
   pinMode(BUZZER2, OUTPUT);
   digitalWrite(BUZZER2, LOW);
@@ -137,7 +149,7 @@ void setup()
 
   //wifiClient.connect(serverIP, SERVER_PORT);
 
-  // Configure nano to run at 115200bps
+  // Configure RFID module to communicate at 115200bps
   if (!setupNano(115200))
   {
     Serial.println("Module not connected!");
@@ -150,6 +162,8 @@ void setup()
   startReading();
 }
 
+// Enable continuous scan mode
+// Return false if continuous scan mode is already enabled, true otherwise
 bool startReading()
 {
   if (!gateReading)
@@ -161,6 +175,8 @@ bool startReading()
   return false;
 }
 
+// Disable continuous scan mode
+// Return false if continuous scan mode is already disabled, true otherwise
 bool stopReading()
 {
   if (gateReading)
@@ -173,6 +189,8 @@ bool stopReading()
   return false;
 }
 
+// Write a product ID and serial number to a tag (should be held in front of antenna)
+// Return true if successfully written, false otherwise
 bool writeTag(uint32_t productId, uint32_t productSerialNumber)
 {
   char buffer[12] = { 0 };
@@ -180,7 +198,7 @@ bool writeTag(uint32_t productId, uint32_t productSerialNumber)
   memcpy(buffer + sizeof(productId), &productSerialNumber, sizeof(productSerialNumber));
 
   bool wasReading = stopReading();
-  bool ret = nano.writeTagEPC(buffer, sizeof(buffer), 5000) == RESPONSE_SUCCESS;
+  bool ret = nano.writeTagEPC(buffer, sizeof(buffer), TAG_WRITE_TIMEOUT) == RESPONSE_SUCCESS;
 
   if (wasReading)
     startReading();
@@ -188,20 +206,12 @@ bool writeTag(uint32_t productId, uint32_t productSerialNumber)
   return ret;
 }
 
-const char* skipWhitespace(const char* str) {
-  if (!str)
-    return str;
-  
-  while (*str && *str == ' ')
-    ++str;
-    
-  return str;
-}
-
+// Main loop
 void loop()
 {
   static char inputBuffer[33];
 
+  // Check if there is data available on the debug port (USB-C)
   if (Serial.available())
   {
     size_t readCount = Serial.readBytesUntil('\n', inputBuffer, sizeof(inputBuffer) - 1);
@@ -218,11 +228,19 @@ void loop()
       while (c != -1 && c != '\n');
     }
 
-    Serial.println(inputBuffer);
+    Serial.println(inputBuffer); // debug purposes
 
     uint32_t productId = 0;
     uint32_t productSerialNumber = 0;
     long gain = 0;
+
+    // The debug interface supports three commands:
+    // 1) Write data to a tag:
+    //    write <product_id> <product_serial_number>
+    // 2) Set antenna gain (up to 2700):
+    //    gain <antenna_gain>
+    // 3) Enable debug output from RFID module:
+    //    debug
 
     if (sscanf(inputBuffer, "write %u %u", &productId, &productSerialNumber) == 2)
     {
@@ -270,6 +288,7 @@ void loop()
       Serial.println("Unknown command!");
   }
 
+  // Check if there is tag data available
   if (gateReading && nano.check())
   {
     uint8_t responseType = nano.parseResponse();
@@ -281,12 +300,23 @@ void loop()
 
       uint32_t productId;
       uint32_t productSerialNumber;
+
       memcpy(&productId, epcStart, sizeof(productId));
       memcpy(&productSerialNumber, epcStart + sizeof(productId), sizeof(productSerialNumber));
 
-      if (tagCache.insert(productId, productSerialNumber))
+      // When the SRTR operates in continuous scan mode, it will repeatedly
+      // scan the same tag as long as it remains within range of the antenna.
+      //
+      // To deal with this undesirable behaviour, we cache the first scan
+      // of a tag that occurs and ignore subsequent scans if the interval
+      // between scans is less than TAG_READ_TIMEOUT.
+      //
+      // The tag cache is used to store cached tags. Insertion will fail if the
+      // same tag information was already recently inserted less than TAG_READ_TIMEOUT
+      // milliseconds ago.
+      if (tagCache.insert(productId, productSerialNumber, TAG_READ_TIMEOUT))
       {
-        tone(BUZZER1, 1800, 100);
+        tone(BUZZER1, 1800, 100); // sound buzzer
         Serial.print("Product ID: ");
         Serial.print(productId);
         Serial.print(" Product serial number: ");
@@ -300,8 +330,8 @@ void loop()
   //   Serial.print("Tag cache size: ");
   //   Serial.print(tagCache.size());
   //   Serial.println();
-  //   sendProductCodes();
-  //   Serial.println("Sent product codes!");
+  //   sendProductInfo();
+  //   Serial.println("Sent product info!");
   //   lastMessageTime = millis();
   // }
 }
--- device/RFIDGate/TagCache.h
+++ device/RFIDGate/TagCache.h
@@ -1,51 +1,68 @@
 #ifndef __TAG_CACHE_H__
 #define __TAG_CACHE_H__
 
+// This file defines a basic data structure for caching scanned tag information.
+
 #include "Arduino.h"
 
+// Length, in bytes, of the EPC field on a tag
 #define EPC_LENGTH 12
 
 template <size_t Size>
 class TagCache
 {
+  // Tag record
   struct Record
   {
-    uint32_t productId;
-    uint32_t productCode;
-    unsigned long timestamp;
-    bool valid = false;
+    uint32_t productId;           // ID used to determine product type
+    uint32_t productSerialNumber; // ssed to differentiate between items of same product type
+    unsigned long timestamp;      // time when tag was scanned
+    bool valid = false;           // whether the record is valid
   };
 
-  size_t _size;     // Number of entries currently cached
-  Record _records[Size];
+  size_t _size;          // number of entries currently cached
+  Record _records[Size]; // record array
 
   public:
 
     TagCache() : _size(0) { };
     
+    // Return the number of records currently stored in the cache
     size_t size() const
     {
       return _size;
     }
 
+    // Return the maximum number of records the cache can hold
     size_t capacity() const
     {
       return Size;
     }
 
-    // Returns true if successfully inserted, false otherwise
+    // Insert a record; return true if successfully inserted, false otherwise.
+    //
+    // 1) If there is space available in the cache and there is no record with the specified
+    // product ID and serial number present, a record will be successfully inserted.
+    //
+    // 2) If there is a record with the same product ID and serial number already present, a new
+    // record will be inserted as long as the time difference between the new record and existing
+    // record exceeds the timeout period.
+    //
+    // For example, if a record was inserted at t = 1000, after which the same record is inserted
+    // at t = 3000, insertion will fail. If however, the same record is inserted at t = 7000, the
+    // insertion will succeed.
     bool insert(uint32_t productId, uint32_t productSerialNumber, unsigned long timeout = 5000)
     {
-      // Index of first empty cache slot
-      ssize_t firstEmpty = -1;
-      size_t recordCount = 0;
+      ssize_t firstEmpty = -1; // keep track of the first empty cache slot
+      size_t recordCount = 0;  // keep track of how many records we've checked
 
       for (size_t i = 0; i < Size; ++i)
       {
         Record& record(_records[i]);
 
-        if (record.productId == productId && record.productCode == productSerialNumber)
+        if (record.productId == productId && record.productSerialNumber == productSerialNumber)
         {
+          // Duplicate record; check timestamp
           unsigned long timestamp = record.timestamp;
           unsigned long newTimestamp = millis();
           record.timestamp = newTimestamp;
@@ -59,22 +76,25 @@ class TagCache
           continue;
         }
         
+        // Cache is full
         if (++recordCount > _size)
           return false;
       }
 
-      // Save tag data in empty record
+      // Save tag data in first empty record
       Record& record(_records[firstEmpty]);
       record.productId = productId;
-      record.productCode = productSerialNumber;
+      record.productSerialNumber = productSerialNumber;
       record.timestamp = millis();
       record.valid = true;
       ++_size;
       return true;
     }
 
-    // Remove an entry at the specified index, and return pointer to epc data,
-    // or null if no entry at index
+    // Remove a record at the specified index, setting productId and productSerialNumber
+    // to point at the record's corresponding fields
+    // Return true if a record was removed, or false if no valid record exists at the
+    // specified index
     bool remove(size_t i, uint32_t* productId, uint32_t* productSerialNumber)
     {
       Record& record(_records[i]);
@@ -84,8 +104,9 @@ class TagCache
 
       record.valid = false;
       --_size;
+      // Replace these assignments with memcpy, because this is so unsafe
       *productId = record.productId;
-      *productSerialNumber = record.productCode;
+      *productSerialNumber = record.productSerialNumber;
       return true;
     }
 };
